2fa6f8605684f9205d61be900a781997896ef246
Krish <krishc8@hotmail.com>
2019-10-23 19:37:54 -0400

    Refactoring Game States and Stages
    This commit introduces the DisplayMode enum which lets us store the
    current game state in a clean and extensible style. This is then
    integrated with the Stages and View code.

--- view.py
+++ view.py
@@ -1,12 +1,35 @@
 import pygame
+from pygame.locals import QUIT
+from enums import DisplayMode
 
 
 class GameView:
-    def __init__(self, is_bot: bool):
-        self.bot_game = is_bot
+    def __init__(self, Game):
+        self.game = Game
+        self.currentMode = DisplayMode.Title
 
+    def title_screen(self):
+        raise NotImplementedError
+    
+    def selection_screen(self):
+        raise NotImplementedError
 
-class SelectionView:
-    def __init__(self, is_bot: bool):
-        self.bot_game = is_bot
+    def game_screen(self):
+        raise NotImplementedError
+
+    def is_over(self):
+        raise NotImplementedError
+
+    def play(self):
         pygame.init()
+        self.display = pygame.display.set_mode((1000, 600))
+        while True:
+            if self.currentMode == DisplayMode.Title:
+                self.title_screen()
+            elif self.currentMode == DisplayMode.Selection:
+                self.selection_screen()
+            elif self.currentMode == DisplayMode.Gameplay:
+                self.game_screen()
+            else:
+                self.is_over()
+            pygame.display.update()
