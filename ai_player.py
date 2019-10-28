b189e09dfd1d8936d780d8475f282046c81b86d8
kevinborja1 <56275941+kevinborja1@users.noreply.github.com>
2019-10-28 15:57:09 -0400

    edit ai player

--- ai_player.py
+++ ai_player.py
@@ -5,38 +5,51 @@ from player import Player
 
 
 class AI(Player):
-
+    moves: queue
+    
     def __init__(self):
         super().__init__(self, "AI Player")
         self.moves = queue.Queue
 
+    def add_ship(self):
+        raise NotImplementedError
+
     def search(self):
-        potential_move = [self.board[row][col] for row in range(8) for col in
-                          range(8) if not self.board[row][col].isHit]
+        potential_move = [[row, col] for row in range(8) for col in
+                          range(8) if not self.board.grid[row][col].isHit]
         if len(potential_move) > 0:
-            self.attack(potential_move[random.randint(len(potential_move))])
+            self.sink(potential_move[random.randint(len(potential_move))][0],
+                                       [random.randint(len(potential_move))][1])
         else:
             # empty potential_moves means game is over
             return None
 
-    def sink(self):
-        potential_hit = self.moves.get()
-        self.attack(potential_hit)
+    def sink(self, row: int, col: int):
+        if self.moves.empty():
+            self.attack(row, col)
+        else:
+            potential_hit = self.moves.get()
+            row, col = potential_hit[0], potential_hit[1]
+        if self.board.grid[row][col].isHit:
+            # will add adjacent tiles to moves
+            for x in [-1, 1]:
+                if x + row in range(8) and not self.board.grid[x + row][col].isHit:
+                    self.moves.put_nowait([x + row, col])
 
-        # will add adjacent tiles to moves
-        for x in [-1, 1]:
-            pass
-        for y in [-1, 1]:
-            pass
+            for y in [-1, 1]:
+                if y + col in range(8) and not self.boar.grid[row][y+ol].isHit:
+                    self.moves.put_nowait([row, y + col])
 
     def move(self):
         if self.moves.empty():
             self.search()
         else:
-            self.sink()
+            self.sink(-1, -1)
 
-    def attack(self, tile):
-        tile.register_hit()
+    def attack(self, row: int, col: int):
+        self.board.add_attack(row, col)
 
     def use_ability(self):
         raise NotImplementedError
+
+
