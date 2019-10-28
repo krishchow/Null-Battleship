4fc4e20599881f0a1b1f2b0025b28e20f88428c5
Krish <krishc8@hotmail.com>
2019-10-28 17:27:55 -0400

    Improving Code Style
    This commit focuses on improving the overall style accross the
    application. By conforming to the flake8 standard in our classes,
    we can make the code more readable.

--- pages/Stages.py
+++ pages/Stages.py
@@ -4,33 +4,37 @@ from util.viewSupport import Button, MyImage
 from util import parameters
 from textbox.pygame_textinput import TextInput
 
+
 class Stage:
     def __init__(self, screen: pygame.Surface, view):
         self.clickables = []
         self.images = []
         self.screen = screen
         self.view = view
-    
+
     def switch_stage(self):
         pass
 
     def render(self):
         raise NotImplementedError
-    
+
     def handle_events(self, events):
         pass
 
+
 class TitlePage(Stage):
     def switch_stage(self):
-        button = Button(300,280, 200,60, 'Player V Player')
-        button.handler = lambda x=self.view: x.switch_stage(DisplayMode.Selection)
-        button2 = Button(300,360, 200,60, 'Player V AI')
-        button2.handler = lambda x=self.view: x.switch_stage(DisplayMode.BotSelection)
+        button = Button(300, 280, 200, 60, 'Player V Player')
+        button.handler = \
+            lambda x=self.view: x.switch_stage(DisplayMode.Selection)
+        button2 = Button(300, 360, 200, 60, 'Player V AI')
+        button2.handler = \
+            lambda x=self.view: x.switch_stage(DisplayMode.BotSelection)
         self.clickables.append(button)
         self.clickables.append(button2)
-        self.images.append(MyImage(0,0, "sprites/island.jpg"))
-        self.images.append(MyImage(600,0, "sprites/pirate.png"))
-        self.images.append(MyImage(30,60, "sprites/battleship.png"))
+        self.images.append(MyImage(0, 0, "sprites/island.jpg"))
+        self.images.append(MyImage(600, 0, "sprites/pirate.png"))
+        self.images.append(MyImage(30, 60, "sprites/battleship.png"))
 
     def render(self):
         for i in self.images:
@@ -45,11 +49,13 @@ class TitlePage(Stage):
                 for i in self.clickables:
                     i.ifClicked(pygame.mouse.get_pos())
 
+
 class GameplayPage(Stage):
     def handle_events(self, events):
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 handleMouseDown(self.view)
+
 
 class SelectionPage(Stage):
     def switch_stage(self):
@@ -58,28 +64,33 @@ class SelectionPage(Stage):
     def render(self):
         self.screen.fill(parameters.colors["white"])
         self.screen.blit(self.tb.get_surface(), (10, 460))
-    
+
     def handle_events(self, events):
         self.tb.update(events)
+
 
 class BotSelectionPage(Stage):
     def render(self):
         self.screen.fill(parameters.colors["red"])
 
+
 class GameOver(Stage):
     def render(self):
         self.screen.fill(parameters.colors["grey"])
+
 
 def handleMouseDown(view) -> None:
     pos = pygame.mouse.get_pos()
 
     # Change the x/y screen coordinates to grid coordinates
-
-    column = pos[0] // (parameters.board_params["cell_width"] + parameters.board_params["margin"])
-    row = pos[1] // (parameters.board_params["cell_height"] + parameters.board_params["margin"])
+    width = parameters.board_params["cell_width"]
+    height = parameters.board_params["cell_height"]
+    margin = parameters.board_params["margin"]
+    column = pos[0] // width + margin
+    row = pos[1] // height + margin
 
     # Set that location to True
     board = view.game.current_board()
     board.grid[row][column].is_hit = True
     print(board.grid)
-    print ('Click ', pos, 'Grid coordinates: ', row, column)
+    print('Click ', pos, 'Grid coordinates: ', row, column)
--- view.py
+++ view.py
@@ -1,23 +1,22 @@
 import pygame
 from pygame import Surface
-from pygame.locals import QUIT
 from util.enums import DisplayMode
 from board import Board
-from player import Player
-from util import parameters
 from util.viewSupport import *
 from pages.Stages import *
+
 
 class GameView:
     screen: Surface
     currentPage: Stage
+    board: Board
 
     def __init__(self, game):
         self.game = game
         self.board = game.current_board()
         self.currentPage = None
         self._running = False        # This is to begin running the game
-        self._keys_pressed = False   #sequence of keys that are pressed
+        self._keys_pressed = False   # sequence of keys that are pressed
         self.screen = None
         self.clock = pygame.time.Clock()
 
@@ -33,14 +32,14 @@ class GameView:
         else:
             self.currentPage = GameOver(self.screen, self)
         self.currentPage.switch_stage()
-    
-    def on_execute(self)-> None:
+
+    def on_execute(self) -> None:
         """
         Run the game until the game ends.
         """
 
         while self._running:
-            #pygame.time.wait(100)
+            # pygame.time.wait(100)
             events = pygame.event.get()
             if events:
                 self.currentPage.handle_events(events)
@@ -59,26 +58,15 @@ class GameView:
         self.switch_stage(DisplayMode.Title)
         self._running = True
 
-    def on_loop(self)-> None:                           ## FIX SELF.PLAYER with actual player once I define
+    def on_loop(self) -> None:                           ## FIX SELF.PLAYER with actual player once I define
         """
         Check for win/lose conditions and game keeps looping till a winner is announced
         """
-        #self._keys_pressed = pygame.key.get_pressed()
-        if (player.number_of_ships == 0) and (board.player != winner()):
-            print("You lose! :( Better luck next time.")
-            #self._running = False
-            #do not set running false right away, the user should quit the game by themselves.
-        elif (player.number_of_ships != 0) and (board.player != winner()):
-            print("Congradulations, you won!")
-            #self._running = False
-            #do not set running false right away, the user should quit the game by themselves.
-        else:
-            pass        
-    
+        raise NotImplementedError
+
     def on_cleanup(self) -> None:
         pygame.quit()
 
     def play(self):
         self.on_init()
         self.on_execute()
-
