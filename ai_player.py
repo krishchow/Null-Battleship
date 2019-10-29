03ace2774d3776814a3ed39da49a99f69cbe0f14
kevinborja1 <56275941+kevinborja1@users.noreply.github.com>
2019-10-28 21:49:31 -0400

    more edits to ai player

--- ai_player.py
+++ ai_player.py
@@ -1,7 +1,9 @@
 import queue
 import random
 
+import ship
 from player import Player
+from util.enums import Direction
 
 
 class AI(Player):
@@ -11,16 +13,21 @@ class AI(Player):
         super().__init__(self, "AI Player")
         self.moves = queue.Queue
 
-    def add_ship(self):
-
-        raise NotImplementedError
+    def add_ship(self, new_ship: ship):
+        direction = [Direction.UP, Direction.Down, Direction.RIGHT,
+                     Direction.LEFT]
+        while True:
+            row, col = random.randint(8), random.randint(8)
+            if not self.board.is_ship(row, col) and self.board.add_ship(row, col
+                    , direction[random.randint(4)], new_ship):
+                break
 
     def search(self):
         potential_move = [[row, col] for row in range(8) for col in
                           range(8) if not self.board.grid[row][col].isHit]
         if len(potential_move) > 0:
             self.sink(potential_move[random.randint(len(potential_move))][0],
-                                       [random.randint(len(potential_move))][1])
+                      [random.randint(len(potential_move))][1])
         else:
             # empty potential_moves means game is over
             return None
@@ -34,11 +41,13 @@ class AI(Player):
         if self.board.grid[row][col].isHit:
             # will add adjacent tiles to moves
             for x in [-1, 1]:
-                if x + row in range(8) and not self.board.grid[x + row][col].isHit:
+                if x + row in range(8) and not self.board.grid[x + row][
+                    col].isHit:
                     self.moves.put_nowait([x + row, col])
 
             for y in [-1, 1]:
-                if y + col in range(8) and not self.boar.grid[row][y+ol].isHit:
+                if y + col in range(8) and not self.boar.grid[row][
+                    y + col].isHit:
                     self.moves.put_nowait([row, y + col])
 
     def move(self):
@@ -52,5 +61,3 @@ class AI(Player):
 
     def use_ability(self):
         raise NotImplementedError
-
-
